ae68af569be48a0b28605e6777c07a7f8c99e2a2
Aswin Timalsina <aswin.timalsina1@gmail.com>
2020-10-29 13:06:33 -0500

    some changes

--- app.py
+++ app.py
@@ -63,12 +63,35 @@ def main():
                 st.write(pie_plot)
                 st.pyplot()
 
-            all_column_names = df.column.to_list()
-            type_of_plot = st.selectbox("Select type of Plot", ["area", "bar", "hist", 'box', "kde"])
+            all_columns_names = df.columns.to_list()
+            type_of_plot = st.selectbox("Select type of Plot", ["area", "bar", "line"])
             selected_columns_names = st.multiselect("Select Columns To Plot", all_columns_names)            
 
             if st.button("Generate Plot"):
-                st.success("Generating Customizable Plot of {} for {}".format(type_of_plot, selected_columns_names))
+                st.success("Generating Customizable Plot of {} for {}".format(type_of_plot, selected_columns_names[0]))
+
+                if type_of_plot == 'area':
+                    cust_data = df[selected_columns_names]
+                    st.area_chart(cust_data)
+
+                elif type_of_plot == 'bar':
+                    cust_data = df[selected_columns_names]
+                    st.bar_chart(cust_data)
+
+                elif type_of_plot == 'line':
+                    cust_data = df[selected_columns_names]
+                    st.line_chart(cust_data)
+
+
+                #custom plot 
+                elif type_of_plot:
+                    cust_plot = df[selected_columns_names].plot(kind=type_of_plot)
+                    st.write(cust_plot)
+                    st.pyplot()
+
+
+
+            
 
     elif choice=='Model Building':
         st.subheader('Building ML Model')
